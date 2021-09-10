3745edd39422bf6e972527e61a96a00f4fc815fa
theksenia <ksu.pm13@gmail.com>
2021-09-11 01:24:29 +0300

    add

--- 2.py
+++ 2.py
@@ -1,20 +1,23 @@
-with open("Random.txt", 'r') as f:
-    data = f.read()
-res = [int(i) for i in data.split()]
-n = []
-for p in res:
-    n.append(p**2)
+def filecub(file_name):
+    with open(file_name, 'r') as f:
+        data = f.read()
+    res = [int(i) for i in data.split()]
+    n = []
+    for p in res:
+        n.append(p ** 2)
+    with open(file_name, 'a') as f:
+        for d in range(0, 1010, 10):
+            f.write(str(n[d - 10:d]) + "\n")
+    return d
 
 
-with open("Random.txt", 'a') as f:
-    for d in range(0,1010,10):
-        f.write(str(n[d-10:d])+"\n")
+print(filecub("Random.txt"))
 
 
 
 
 
-#f.write(str(n[d-10:d])+"\n")
+
 
 
 
